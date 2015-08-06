5d41111e035b5170c4ab252de232d96be7ec3e7a
Michele Clamp <michele.clamp@gmail.com>
2015-08-06 09:47:57 -0400

    More picard stuff

--- scripts/setpath.bash
+++ scripts/setpath.bash
@@ -1,4 +1,4 @@
 
 export POGHOME=./
 
-export CLASSPATH=${POGHOME}:${POGHOME}/classfiles2:${POGHOME}/jars/looks-1.3.1.jar:${POGHOME}/jars/epsgraphics.jar:${POGHOME}/jars/forms-1.1.0.jar:${POGHOME}/jars/pal-cvs.jar
+export CLASSPATH=${POGHOME}:${POGHOME}/classfiles2:${POGHOME}/jars/looks-1.3.1.jar:${POGHOME}/jars/epsgraphics.jar:${POGHOME}/jars/forms-1.1.0.jar:${POGHOME}/jars/pal-cvs.jar:${POGHOME}/jars/picard.jar
