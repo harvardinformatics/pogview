bd7628732e08399fef25cf7bb80fc9daae216565
Michele Clamp <michele.clamp@gmail.com>
2015-08-21 11:22:13 -0400

    Added bamfile command line option

---
+++ src2/pogvue/datamodel/GlobalSettings.java
@@ -0,0 +1,22 @@
+package pogvue.datamodel;
+
+import java.util.Vector;
+import java.util.Hashtable;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class GlobalSettings {
+
+   public static Vector bamfiles = new Vector();
+
+}
+
+
+
